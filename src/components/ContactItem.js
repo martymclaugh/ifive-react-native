bd6cdc6b0219625ca2515edf262f9623f8859cd2
Marty McLaughlin <martymclaugh@gmail.com>
2016-12-05 16:50:45 -0800

    add button to send high five

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -5,13 +5,21 @@ import {
   Text,
   View
 } from 'react-native';
+import styles from '../styles/common-styles.js';
+import Button from './Button'
 
 export default class ContactItem extends Component {
 
   render(){
     return (
-      <View key={this.props.phone_number}>
-        <Text>{this.props.first_name} {this.props.last_name}</Text>
+      <View key={this.props.phone_number} style={styles.contact_item}>
+
+        <Text style={styles.contact_text}>{this.props.first_name} {this.props.last_name}</Text>
+        <Button
+            text="Send High Five"
+            onpress={this.props.onpress}
+            button_styles={styles.transparent_button}
+            button_text_styles={styles.transparent_button_text} />
         <Text>{this.props.phone_number}</Text>
       </View>
     );
