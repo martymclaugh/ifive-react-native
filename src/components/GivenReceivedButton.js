86556ad37910671432725ebc1f5369815c2e5dbc
Marty McLaughlin <martymclaugh@gmail.com>
2016-12-09 12:30:54 -0800

    add component to go to fives history

---
+++ src/components/GivenReceivedButton.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  TouchableHighlight,
+  Text,
+  View
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class HighFiveButton extends Component {
+
+  render(){
+    return (
+      <View>
+        <TouchableHighlight underlayColor={"#E8E8E8"} onPress={this.props.onpress}>
+          <View>
+              <Text style={this.props.style}><Icon name="hand-paper-o" size={20} color="black" />  {this.props.text}: {this.props.number}</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
